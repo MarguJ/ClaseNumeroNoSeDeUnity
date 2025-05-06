1f6c72e325ea56bd91244b4518d27c986588a2cb
MarguJ <margulesjoa@gmail.com>
2025-05-06 12:13:56 -0300

    Final

---
+++ Assets/Script/SaludoUIManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SaludoUIManager : MonoBehaviour
+{
+    public TextMeshProUGUI saludo;
+    public TMP_InputField inputNombre;
+    string nombreAIngresar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        saludo.text = "";
+    }
+
+    public void saludar()
+    {
+        nombreAIngresar = inputNombre.text;
+        saludo.text = "Hola " + nombreAIngresar;
+    }
+}
