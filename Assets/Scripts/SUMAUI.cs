873949c779d7dd207448fb8d64fad2c0b6a1a508
MarguJ <margulesjoa@gmail.com>
2025-05-09 09:05:56 -0300

    Mitad terminado

---
+++ Assets/Scripts/SUMAUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class SUMAUI : MonoBehaviour
+{
+    public TextMeshProUGUI resultado;
+    public TMP_InputField INumero1;
+    public TMP_InputField INumero2;
+    int num1;
+    int num2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        resultado.text = "";
+    }
+
+    public void SumarYMostrar()
+    {
+        num1 = int.Parse(INumero1.text);
+        num2 = int.Parse(INumero2.text);
+        resultado.text = (num1 + num2).ToString();
+    }
+ }
